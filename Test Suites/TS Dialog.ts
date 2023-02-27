df29f1a247c00aa59a18bc1907eb361545c917e9
Pham Hoang <pham.hoang@hitachivantara.com>
2023-02-27 16:45:28 +0700

    Second commit

---
+++ Test Suites/TS Dialog.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS Dialog</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>5db3948a-6a5f-4328-8912-317e627ca969</testSuiteGuid>
+   <testCaseLink>
+      <guid>cb3dcd3f-9de9-4304-9ff9-6541cec57bbf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TCs Dialog Web Page/TC_01 Verify dialog title</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
